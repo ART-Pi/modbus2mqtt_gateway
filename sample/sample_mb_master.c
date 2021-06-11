fa840b6cd84e0904f9461a6b6f2928f6d66906a7
Sherman <shaopengyu@rt-thread.com>
2021-06-11 18:59:17 +0800

    [add] 添加示例代码、说明、操作手册

---
+++ sample/sample_mb_master.c
@@ -0,0 +1,136 @@
+/*
+ * Copyright (c) 2006-2018, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2019-06-21     flybreak     first version
+ */
+
+#include <rtthread.h>
+#include <rtdevice.h>
+
+#include "drv_common.h"
+#include "mb.h"
+#include "mb_m.h"
+#include "user_mb_app.h"
+
+#ifdef PKG_MODBUS_MASTER_SAMPLE
+#define SLAVE_ADDR      MB_SAMPLE_TEST_SLAVE_ADDR
+#define PORT_NUM        MB_MASTER_USING_PORT_NUM
+#define PORT_BAUDRATE   MB_MASTER_USING_PORT_BAUDRATE
+#else
+#define SLAVE_ADDR      0x01
+#define PORT_NUM        3
+#define PORT_BAUDRATE   115200
+#endif
+#define PORT_PARITY     MB_PAR_EVEN
+
+#define MB_POLL_THREAD_PRIORITY  10
+#define MB_SEND_THREAD_PRIORITY  RT_THREAD_PRIORITY_MAX - 1
+
+#define MB_SEND_REG_START  2
+#define MB_SEND_REG_NUM    3
+
+#define MB_POLL_CYCLE_MS   500
+
+#define LED_RED GET_PIN(C,15)
+
+extern USHORT   usMRegHoldBuf[MB_MASTER_TOTAL_SLAVE_NUM][M_REG_HOLDING_NREGS]; // modbus master 用全局二维数组来记录多个从机的数据
+
+static void send_thread_entry(void *parameter)
+{
+    eMBMasterReqErrCode error_code = MB_MRE_NO_ERR;
+    rt_uint16_t error_count = 0;
+    USHORT data[2] = {0};
+
+    rt_pin_mode(LED_RED, PIN_MODE_OUTPUT);
+
+
+    while (1)
+    {
+        /* Test Modbus Master */
+        data[0] = (USHORT)(rt_tick_get() / 10);
+        data[1] = (USHORT)(rt_tick_get() % 10);
+
+        error_code = eMBMasterReqWriteMultipleHoldingRegister(SLAVE_ADDR,          /* salve address */
+                                                              MB_SEND_REG_START,   /* register start address */
+                                                              MB_SEND_REG_NUM,     /* register total number */
+                                                              data,                /* data to be written */
+                                                              RT_WAITING_FOREVER); /* timeout */
+                                                              
+        error_code = eMBMasterReqWriteMultipleHoldingRegister(SLAVE_ADDR,          /* salve address */
+                                                              MB_SEND_REG_START,   /* register start address */
+                                                              MB_SEND_REG_NUM,     /* register total number */
+                                                              data,                /* data to be written */
+                                                              RT_WAITING_FOREVER); /* timeout */
+
+        error_code = eMBMasterReqReadHoldingRegister(SLAVE_ADDR,          /* salve address */
+                                        1,   /* register start address */
+                                        1,     /* register total number */
+                                        RT_WAITING_FOREVER);
+        rt_pin_write(LED_RED, !usMRegHoldBuf[0][1]);
+
+        /* Record the number of errors */
+        if (error_code != MB_MRE_NO_ERR)
+        {
+            error_count++;
+        }
+    }
+}
+
+static void mb_master_poll(void *parameter)
+{
+    eMBMasterInit(MB_RTU, PORT_NUM, PORT_BAUDRATE, PORT_PARITY);
+    eMBMasterEnable();
+
+    while (1)
+    {
+        eMBMasterPoll();
+        rt_thread_mdelay(MB_POLL_CYCLE_MS);
+    }
+}
+
+static int mb_master_samlpe(int argc, char **argv)
+{
+    static rt_uint8_t is_init = 0;
+    rt_thread_t tid1 = RT_NULL, tid2 = RT_NULL;
+
+    if (is_init > 0)
+    {
+        rt_kprintf("sample is running\n");
+        return -RT_ERROR;
+    }
+    tid1 = rt_thread_create("md_m_poll", mb_master_poll, RT_NULL, 512, MB_POLL_THREAD_PRIORITY, 10);
+    if (tid1 != RT_NULL)
+    {
+        rt_thread_startup(tid1);
+    }
+    else
+    {
+        goto __exit;
+    }
+
+    tid2 = rt_thread_create("md_m_send", send_thread_entry, RT_NULL, 512, MB_SEND_THREAD_PRIORITY, 10);
+    if (tid2 != RT_NULL)
+    {
+        rt_thread_startup(tid2);
+    }
+    else
+    {
+        goto __exit;
+    }
+
+    is_init = 1;
+    return RT_EOK;
+
+__exit:
+    if (tid1)
+        rt_thread_delete(tid1);
+    if (tid2)
+        rt_thread_delete(tid2);
+
+    return -RT_ERROR;
+}
+MSH_CMD_EXPORT(mb_master_samlpe, run a modbus master sample);
